f8dbefbd13fe54e1b1a861082ff20e8a7ff2977c
siliconvalley4066 <90234329+siliconvalley4066@users.noreply.github.com>
2024-07-03 17:37:46 +0900

    fix PWM max to 50%

--- GOscillo/GOscillo.ino
+++ GOscillo/GOscillo.ino
@@ -1,5 +1,5 @@
 /*
- * STM32F103C8T6 Oscilloscope using a 128x64 OLED Version 1.06
+ * STM32F103C8T6 Oscilloscope using a 128x64 OLED Version 1.07
  * The max DMA sampling rates is 5.14Msps with single channel, 2.57Msps with 2 channels.
  * The max software loop sampling rates is 100ksps with 2 channels.
  * + Pulse Generator
@@ -755,6 +755,7 @@ void loadEEPROM() { // Read setting values from EEPROM (abnormal values will be 
   if (ifreq > 999999L) ++error;
   time_mag = EEPROM.read(p++);               // magnify timebase
   if (error > 0) {
+    EEPROM.format();
     set_default();
   }
 }
